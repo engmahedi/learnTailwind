d4bc4b9272cbfa8ea1317a163bc816c16a91ad3b
Mahedi <faruk585@gmail.com>
2023-04-03 20:08:07 +0600

    header start

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    screen: {
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px'
+    },
+    container: {
+      center: true,
+      padding:'1rem'
+    },
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        Roboto: ["Roboto","sans-serif"]
+      },
+      colors: {
+        'primary':'#FD3D57'
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
